4b4847752cf981da4de916c08981a234b974e63b
Dan Paik <danpaik@gmail.com>
2022-07-18 16:23:50 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "mgmt_ip" {
+output "Internal IP" {
  # value = google_compute_instance.demo.network_interface.0.access_config.0.nat_ip
 
   value = var.gpu_use == true ? google_compute_instance.gpu[0].network_interface.0.network_ip : google_compute_instance.non_gpu[0].network_interface.0.network_ip
